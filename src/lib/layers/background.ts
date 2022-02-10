e4aef8309ff54399ff09777d52f3d3e4a3a13d49
Piotr Praga <piotr.praga@webfleet.com>
2022-02-10 23:16:59 +0100

    Change game scale

--- src/lib/layers/background.ts
+++ src/lib/layers/background.ts
@@ -9,12 +9,11 @@ export default class Background extends Layer {
         const { game } = this
         const { ctx, resolution } = game
         const { camera } = game.getCurrentScene()
-        const bg1 = game.getImage('bg1.png')
-        const bg2 = game.getImage('bg2.png')
+        const bg = game.getImage('bg2.png')
         const moon = game.getImage('moon.png')
         const clouds = game.getImage('clouds.png')
         const s = camera.pos.x / 1.5
-        const w1 = bg2.width - 1
+        const w1 = bg.width - 1
         const x1 = s + Math.round((-s + camera.focus.x) / w1) * w1
         const y1 = 365 + camera.pos.y
         const w2 = clouds.width - 1
@@ -23,11 +22,12 @@ export default class Background extends Layer {
 
         this.scroll = this.scroll < w2 ? this.scroll + 0.1 : 0
 
-        ctx.drawImage(bg1, 0, -16 + camera.pos.y / 16)
+        ctx.fillStyle = '#47038A'
+        ctx.fillRect(0, 0, resolution.x, resolution.y)
         ctx.drawImage(moon, resolution.x - 70, 6)
         ctx.drawImage(clouds, x2 - w2, y2)
         ctx.drawImage(clouds, x2, y2)
-        ctx.drawImage(bg2, x1 - w1, y1)
-        ctx.drawImage(bg2, x1, y1)
+        ctx.drawImage(bg, x1 - w1, y1)
+        ctx.drawImage(bg, x1, y1)
     }
 }
